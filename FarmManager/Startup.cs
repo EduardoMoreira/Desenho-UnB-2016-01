b66e0bec2b95ad7ac051952939a332ba20b5c30f
Eduardo Moreira <moreira.edu@hotmail.com>
2016-04-06 14:12:56 -0300

    Criando projeto ASP.NET MVC.

---
+++ FarmManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FarmManager.Startup))]
+namespace FarmManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
