08f4ea026e0944cb415e7215a69571a7206129a5
ebenezerandrade <ebenezer.andrade1@gmail.com>
2016-05-18 20:31:50 -0300

    Insercao dos campos nas Models e refatoracao das views

--- FarmManager/Models/EquipamentoRural.cs
+++ FarmManager/Models/EquipamentoRural.cs
@@ -12,21 +12,20 @@ namespace FarmManager.Models
         public int CDEquipamentoRural { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:#.##}")]
-        [Required(ErrorMessage = "Digite a data de contratação do funcionário!")]
-        [Display(Name = "Data de Contratação")]
+        [Required(ErrorMessage = "Digite a data o valor de aquisição!")]
+        [Display(Name = "Valor de aquisição")]
         public decimal VLCompra { get; set; }
 
-        [Required(ErrorMessage = "Digite a data de contratação do funcionário!")]
-        [Display(Name = "Data de Contratação")]
+        [Required(ErrorMessage = "Digite o nome do equipamento!")]
+        [Display(Name = "Nome do equipamento")]
         public string DEEquipamentoRural { get; set; }
 
-        [Required(ErrorMessage = "Digite a data de contratação do funcionário!")]
-        [Display(Name = "Data de Contratação")]
+        [Display(Name = "Equipamento alugado")]
         public bool STAlugado { get; set; }
 
         [DataType(DataType.Date)]
-        [Required(ErrorMessage = "Digite a data de contratação do funcionário!")]
-        [Display(Name = "Data de Contratação")]
+        [Required(ErrorMessage = "Digite a data de aquisição do equipamento!")]
+        [Display(Name = "Data de aquisição")]
         public DateTime DTCompra { get; set; }
     }
 }
